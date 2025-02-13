eeea61c355d7aeb7cd806b4f625f1e0869d71478
ShinYounHo <suh2963@gmail.com>
2025-02-13 12:30:19 +0900

    type define

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -1,3 +1,22 @@
+type PostDto = {
+  id: number;
+  createdDate: string;
+  modifiedDate: string;
+  authorId: number;
+  authorName: string;
+  title: string;
+  published: boolean;
+  listed: boolean;
+};
+
+type PostItemPageDto = {
+  currentPageNo: number;
+  pageSize: number;
+  totalPages: number;
+  totalItems: number;
+  items: PostDto[];
+};
+
 export default async function Page() {
   const response = await fetch("http://localhost:8080/api/v1/posts");
 
@@ -6,7 +25,7 @@ export default async function Page() {
   }
 
   const rsData = await response.json();
-  const pageDto = rsData.data;
+  const pageDto: PostItemPageDto = rsData.data;
 
   return (
     <div>
@@ -23,15 +42,15 @@ export default async function Page() {
       <hr />
 
       <ul>
-        {pageDto.items.map((item: any) => {
+        {pageDto.items.map((item: PostDto) => {
           return (
             <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
               <div>id : {item.id}</div>
               <div>title : {item.title}</div>
               <div>authorId : {item.authorId}</div>
               <div>authorName : {item.authorName}</div>
-              <div>published : {item.published}</div>
-              <div>listed : {item.listed}</div>
+              <div>published : {`${item.published}`}</div>
+              <div>listed : {`${item.listed}`}</div>
             </li>
           );
         })}
