13b58eca801f5409cb54e0f67437d6aa6f2cd0a7
ShinYounHo <suh2963@gmail.com>
2025-02-13 12:16:18 +0900

    post list rendergin

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -6,6 +6,7 @@ export default async function Page() {
   }
 
   const rsData = await response.json();
+  const pageDto = rsData.data;
 
   return (
     <div>
@@ -14,11 +15,26 @@ export default async function Page() {
       <div>응답 코드 : {rsData.code}</div>
       <div>결과 메시지 : {rsData.msg}</div>
 
+      <div>totalPages : {pageDto.totalPages}</div>
+      <div>totalItems : {pageDto.totalItems}</div>
+      <div>currentPageNo : {pageDto.currentPageNo}</div>
+      <div>pageSize : {pageDto.pageSize}</div>
+
       <hr />
 
       <ul>
-        <li>글1</li>
-        <li>글2</li>
+        {pageDto.items.map((item: any) => {
+          return (
+            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
+              <div>id : {item.id}</div>
+              <div>title : {item.title}</div>
+              <div>authorId : {item.authorId}</div>
+              <div>authorName : {item.authorName}</div>
+              <div>published : {item.published}</div>
+              <div>listed : {item.listed}</div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
