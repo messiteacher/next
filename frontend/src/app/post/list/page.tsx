2184712d292896957d5f57bd87482be6a5e6b393
ShinYounHo <suh2963@gmail.com>
2025-02-13 12:40:02 +0900

    schema.d.ts type use

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -1,21 +1,7 @@
-type PostDto = {
-  id: number;
-  createdDate: string;
-  modifiedDate: string;
-  authorId: number;
-  authorName: string;
-  title: string;
-  published: boolean;
-  listed: boolean;
-};
+import { components } from "@/lib/backend/apiV1/schema";
 
-type PostItemPageDto = {
-  currentPageNo: number;
-  pageSize: number;
-  totalPages: number;
-  totalItems: number;
-  items: PostDto[];
-};
+type PostDto = components["schemas"]["PostDto"];
+type PostItemPageDto = components["schemas"]["PageDto"];
 
 export default async function Page() {
   const response = await fetch("http://localhost:8080/api/v1/posts");
@@ -42,7 +28,7 @@ export default async function Page() {
       <hr />
 
       <ul>
-        {pageDto.items.map((item: PostDto) => {
+        {pageDto.items?.map((item: PostDto) => {
           return (
             <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
               <div>id : {item.id}</div>
