2e4ccb09de46878c246007f1677d1ddba48caae7
ShinYounHo <suh2963@gmail.com>
2025-02-17 14:59:00 +0900

    middleware auth check

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,32 +3,56 @@ import client from "./lib/backend/client";
 import { cookies } from "next/headers";
 
 export async function middleware(request: NextRequest) {
-  const reqToken = request.cookies.get("accessToken");
-  console.log(request.nextUrl.toString());
+  const myCookies = await cookies();
+  const accessToken = myCookies.get("accessToken");
 
-  if (request.nextUrl.pathname.startsWith("/post/edit/")) {
-    return new NextResponse("로그인이 필요합니다.", {
-      status: 401,
+  let isExpired = true;
+  let payload = null;
+
+  if (accessToken) {
+    try {
+      const tokenParts = accessToken.value.split(".");
+      payload = JSON.parse(Buffer.from(tokenParts[1], "base64").toString());
+      const expTimestamp = payload.exp * 1000; // exp는 초 단위이므로 밀리초로 변환
+      isExpired = Date.now() > expTimestamp;
+      console.log("토큰 만료 여부:", isExpired);
+    } catch (e) {
+      console.error("토큰 파싱 중 오류 발생:", e);
+    }
+  }
+  let isLogin = payload !== null;
+  console.log("------------------");
+  console.log(isLogin, isExpired);
+  if (isLogin && isExpired) {
+    const nextResponse = NextResponse.next();
+    const response = await client.GET("/api/v1/members/me", {
       headers: {
-        "Content-Type": "text/html; charset=utf-8",
+        cookie: (await cookies()).toString(),
       },
     });
+    const springCookie = response.response.headers.getSetCookie();
+    nextResponse.headers.set("set-cookie", String(springCookie));
+    return nextResponse;
   }
 
-  const nextResponse = NextResponse.next();
-
-  const response = await client.GET("/api/v1/members/me", {
-    headers: {
-      cookie: (await cookies()).toString(),
-    },
-  });
-
-  const springCookie = response.response.headers.getSetCookie();
-  nextResponse.headers.set("set-cookie", String(springCookie));
-
-  return nextResponse;
+  if (!isLogin && isProtectedRoute(request.nextUrl.pathname)) {
+    return createUnauthorizedResponse();
+  }
+}
+function isProtectedRoute(pathname: string): boolean {
+  return (
+    pathname.startsWith("/post/write") || pathname.startsWith("/post/edit")
+  );
 }
 
+function createUnauthorizedResponse(): NextResponse {
+  return new NextResponse("로그인이 필요합니다.", {
+    status: 401,
+    headers: {
+      "Content-Type": "text/html; charset=utf-8",
+    },
+  });
+}
 export const config = {
-  matcher: "/:path*",
+  matcher: "/((?!.*\\.|api\\/).*)",
 };
