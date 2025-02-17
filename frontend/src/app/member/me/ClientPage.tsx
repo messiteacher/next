a345f76a7ea03d94eff0e7107b02174fae2615fc
ShinYounHo <suh2963@gmail.com>
2025-02-17 16:25:56 +0900

    change api to token payload

--- frontend/src/app/member/me/ClientPage.tsx
+++ frontend/src/app/member/me/ClientPage.tsx
@@ -12,8 +12,6 @@ export default function ClinetPage({
       <div>내정보 페이지</div>
       <div>번호 : {me.id}</div>
       <div>닉네임 : {me.nickname}</div>
-      <div>가입일자 : {me.createdDate}</div>
-      <div>수정일자 : {me.modifiedDate}</div>
     </>
   );
 }
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,39 @@
 import { NextResponse, type NextRequest } from "next/server";
 import client from "./lib/backend/client";
 import { cookies } from "next/headers";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
 export async function middleware(request: NextRequest) {
   const myCookies = await cookies();
   const accessToken = myCookies.get("accessToken");
+  const { isLogin, isExpired, payload } = parseAccessToken(accessToken);
 
+  if (isLogin && isExpired) {
+    return refreshAccessToken();
+  }
+
+  if (!isLogin && isProtectedRoute(request.nextUrl.pathname)) {
+    return createUnauthorizedResponse();
+  }
+}
+
+async function refreshAccessToken() {
+  const nextResponse = NextResponse.next();
+
+  const response = await client.GET("/api/v1/members/me", {
+    headers: {
+      cookie: (await cookies()).toString(),
+    },
+  });
+
+  const springCookie = response.response.headers.getSetCookie();
+
+  nextResponse.headers.set("set-cookie", String(springCookie));
+
+  return nextResponse;
+}
+
+function parseAccessToken(accessToken: RequestCookie | undefined) {
   let isExpired = true;
   let payload = null;
 
@@ -15,30 +43,16 @@ export async function middleware(request: NextRequest) {
       payload = JSON.parse(Buffer.from(tokenParts[1], "base64").toString());
       const expTimestamp = payload.exp * 1000; // exp는 초 단위이므로 밀리초로 변환
       isExpired = Date.now() > expTimestamp;
-      console.log("토큰 만료 여부:", isExpired);
     } catch (e) {
       console.error("토큰 파싱 중 오류 발생:", e);
     }
   }
-  let isLogin = payload !== null;
-  console.log("------------------");
-  console.log(isLogin, isExpired);
-  if (isLogin && isExpired) {
-    const nextResponse = NextResponse.next();
-    const response = await client.GET("/api/v1/members/me", {
-      headers: {
-        cookie: (await cookies()).toString(),
-      },
-    });
-    const springCookie = response.response.headers.getSetCookie();
-    nextResponse.headers.set("set-cookie", String(springCookie));
-    return nextResponse;
-  }
 
-  if (!isLogin && isProtectedRoute(request.nextUrl.pathname)) {
-    return createUnauthorizedResponse();
-  }
+  let isLogin = payload !== null;
+
+  return { isLogin, isExpired, payload };
 }
+
 function isProtectedRoute(pathname: string): boolean {
   return (
     pathname.startsWith("/post/write") || pathname.startsWith("/post/edit")
